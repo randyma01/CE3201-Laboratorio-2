988ce9925a1e904951096d2462bc92bec17375ff
Randy Martínez <randyma01@gmail.com>
2021-08-19 00:43:39 -0600

    experimento 1: ajustando testbench && simulaciones

--- experimento-1/conversor_bdc_tb.sv
+++ experimento-1/conversor_bdc_tb.sv
@@ -1,29 +1,26 @@
 module conversor_bdc_tb();
-  logic switches, display;
+  logic [3:0] switches; 
+  logic [13:0] display;
   
   conversor_bdc test(switches, display);
   
   initial begin
     $display("Start testbench !!!");
 	 
-	 //switches = 4'b0111 = decimal -> 7;
-	 switches=7;
-	 assert (display === 14'b11111110001111) else $display("SUCCESS: #7");
+	 switches = 4'b0111; // 7;
+	 assert (display === 14'b11110001111111) else $display("SUCCESS: #7");
 	 #10;
 	 
-	 //switches=4'b1001  = decimal -> 9;
-	 switches=9;
-	 assert (display === 14'b11111110001100) else $display("SUCCESS: #9");
+	 switches=4'b1001; // 9;
+	 assert (display === 14'b00110001111111) else $display("SUCCESS: #9");
 	 #10;
 	 
-	 //switches=4'b1100  = decimal -> 12;
-	 switches=12;
-	 assert (display === 14'b10011110010011) else $error("FAILED: #12");
+	 switches=4'b1100; // 12;
+	 assert (display !== 14'b01001001111001) else $error("FAILED: #12");
 	 #10;
 	 
-	 //switches=4'b0011  = decimal -> 3;
-	 switches=3;
-	 assert (display === 14'b1111111000111) else $error("FAILED: #3");
+	 switches=4'b0011; // 3;
+	 assert (display !== 14'b01100001111111) else $error("FAILED: #3");
 	 #10;
 	end
 	
--- experimento-1/simulation/modelsim/experimento-1_run_msim_rtl_verilog.do.bak11
+++ experimento-1/simulation/modelsim/experimento-1_run_msim_rtl_verilog.do.bak11
@@ -5,9 +5,9 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -sv -work work +incdir+C:/Users/ranmsy/Desktop/laboratorio-2-codigos/experimento-1 {C:/Users/ranmsy/Desktop/laboratorio-2-codigos/experimento-1/conversor_bdc.sv}
+vlog -sv -work work +incdir+C:/Users/ranmsy/Desktop/CE3201-Laboratorio-2/experimento-1 {C:/Users/ranmsy/Desktop/CE3201-Laboratorio-2/experimento-1/conversor_bdc.sv}
 
-vlog -sv -work work +incdir+C:/Users/ranmsy/Desktop/laboratorio-2-codigos/experimento-1 {C:/Users/ranmsy/Desktop/laboratorio-2-codigos/experimento-1/conversor_bdc_tb.sv}
+vlog -sv -work work +incdir+C:/Users/ranmsy/Desktop/CE3201-Laboratorio-2/experimento-1 {C:/Users/ranmsy/Desktop/CE3201-Laboratorio-2/experimento-1/conversor_bdc_tb.sv}
 
 vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclonev_ver -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L rtl_work -L work -voptargs="+acc"  conversor_bdc_tb
 
